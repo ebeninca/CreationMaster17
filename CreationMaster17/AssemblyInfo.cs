3a6362df164de0f7365400e8ee50cb060d9efefa
eduardo.beninca <eduardo.beninca@dataprev.gov.br>
2023-10-07 05:20:21 +0100

    renamed everything from CM15 to CM17

--- CreationMaster17/AssemblyInfo.cs
+++ CreationMaster17/AssemblyInfo.cs
@@ -1,14 +1,14 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyFileVersion("15.1.0.0")]
+[assembly: AssemblyFileVersion("17.1.0.0")]
 [assembly: AssemblyTrademark("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("95d11b97-1061-4921-a16d-af65913d4f99")]
 [assembly: AssemblyCopyright("Copyright ©  2010")]
-[assembly: AssemblyTitle("CreationMaster15 (2021)")]
+[assembly: AssemblyTitle("CreationMaster17 (2023)")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CreationMaster15 (2021)")]
-[assembly: AssemblyVersion("15.1.0.0")]
+[assembly: AssemblyProduct("CreationMaster17 (2023)")]
+[assembly: AssemblyVersion("17.1.0.0")]
